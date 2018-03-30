f037120607530c7fe9ec9dea3548b182a4ccce55
AleenaRehman <aleenarehman11.ar@gmail.com>
2018-03-31 00:42:40 +0500

    initial commit

---
+++ src/Testcases/FailTest1.java
@@ -0,0 +1,32 @@
+package Testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.susebox.jtopas.CharArraySource;
+
+public class FailTest1 {
+
+	@Test
+	public void test() throws Exception {
+		char [] data = {'T','e','s','t','i','n','g'};
+		CharArraySource chartest = new CharArraySource(data,0,4);
+		
+		int result1 = chartest.read(data, 2, 4);
+		assertEquals(result1,-1);
+		
+		char result2 = chartest.charAt(3);
+		assertNotEquals(result2,data[3]);
+		
+		int result3 = chartest.length();
+		assertNotEquals(result3,0);
+		
+		CharSequence result4 = chartest.subSequence(1, 4);
+		assertNotEquals(result4,"test");
+		
+		String result5 = chartest.toString();
+		assertNotEquals(result5,null);
+	}
+
+}
---
+++ src/Testcases/FailTests.java
@@ -0,0 +1,11 @@
+package Testcases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AllTests1.class, FailTest1.class })
+public class FailTests {
+
+}
