6193a4076649a1629f499620afd744c34db46d68
AleenaRehman <aleenarehman11.ar@gmail.com>
2018-03-30 23:28:22 +0500

    initial commit

---
+++ src/Testcases/AllTests1.java
@@ -0,0 +1,14 @@
+package Testcases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ExtExceptionsTestSuite.class, JtopassTestSuit.class, NoCaseSequenceStoreTestSuite.class,
+		PatternMatchetTestSuite.class, SequenceStoreTestSuite.class, StandardKeywordHandlerTestSuite.class,
+		StandardSepratorHandlerTestSuite.class, StandardSequenceHandlerTestSuite.class,
+		StandardWhiteSpaceHandlerTestSuite.class })
+public class AllTests1 {
+
+}
